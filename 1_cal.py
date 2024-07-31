7b011f0a1a01e9aebb4979b0fbc2f1f575b6c5f9
kishore-learner <kishorenair001@gmail.com>
2024-07-31 16:51:20 +0530

    pushing first task (primary calculator)

---
+++ 1_cal.py
@@ -0,0 +1,24 @@
+n = int(input("enter 1st digit : "))
+m = int(input("enter 2nd digit : "))
+s = str(input("enter the function [+,x,%,/,^,//] : "))
+ans = int
+if s == "+":
+    ans = n+m
+elif s=="x":
+    ans = n*m
+elif s=="%":
+    ans = n%m
+elif s=="/":
+    ans = n/m
+elif s=="//":
+    ans = n//m
+elif s=="^":
+    ans = n^m
+else:
+    print("invalid")
+print(ans)
+
+
+'''
+this project is simple one.. I know!! this is did in order to recall how to use version control using git 
+'''
