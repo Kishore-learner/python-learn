76a6db47192953e80f40d69ef9751ff66153c56e
kishore-learner <kishorenair001@gmail.com>
2024-08-11 20:20:13 +0530

    string methods

---
+++ stringmethods.py
@@ -0,0 +1,18 @@
+string = "   kishore"
+print(string.find('e'))
+print(string.split('o'))
+print(string.capitalize())
+print(string.upper())
+print(string.isdigit())
+print(string.isalpha())
+print(string.isalnum())
+print(string.lower())
+print(string.count('o'))
+print(string.replace('o','a'))
+print(string.startswith('l'))
+print(string.rstrip())
+print(string.lstrip())
+print(string.strip())
+print(string.zfill(16)) # translates the word into a specific sized word
+
+
